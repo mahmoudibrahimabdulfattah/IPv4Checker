bfee706b3ecd5bb85ffd8ef3bb05fd1eda955d4f
dev7odaa <mah.khalil22@gmail.com>
2025-04-04 19:44:33 +0200

    remove comments

--- src/isValidIPv4.kt
+++ src/isValidIPv4.kt
@@ -1,28 +1,20 @@
 fun isValidIPv4(ip: String): Boolean {
-    // Split Address by dot
+
     val segments = ip.split(".")
 
-    // is contained 4 parts
     if (segments.size != 4) {
         return false
     }
 
-    // check every part
     for (segment in segments) {
         if (segment.isEmpty()) {
             return false
         }
-
-        // Shouldn't contain 0 until 0
         if (segment.length > 1 && segment[0] == '0') {
             return false
         }
-
-        // should contain only digit
         try {
             val value = segment.toInt()
-
-            // is value between 0 and 255?
             if (value < 0 || value > 255) {
                 return false
             }
@@ -30,7 +22,6 @@ fun isValidIPv4(ip: String): Boolean {
             return false
         }
     }
-
     val reconstructed = segments.joinToString(".")
     if (reconstructed != ip) {
         return false
